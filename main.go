7e8f3c0f190bc6d3c3ebe3d49d724eb125c941e1
Shaii <sgolminquentin@gmail.com>
2023-09-14 14:41:58 +0200

    Bases du projet / Utilisation d'IO,OS,FMT.

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	file, err := os.Open("FileConfig.txt")
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
+	defer file.Close()
+
+	data := make([]byte, 1024)
+	for {
+		n, err := file.Read(data)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("File reading error", err)
+			return
+		}
+		fmt.Println("Read", n, "bytes:", string(data[:n]))
+	}
 }
